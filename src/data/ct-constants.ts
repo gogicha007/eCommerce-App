248806ddf7aa1576056904dcbb1a9607ae32182d
gogicha007 <gogicha@gmail.com>
2024-05-11 18:05:12 +0400

    chore: add api variables file

--- src/data/ct-constants.ts
+++ src/data/ct-constants.ts
@@ -1,6 +1,6 @@
-export const CTP_PROJECT_KEY = "essential-items";
-export const CTP_CLIENT_SECRET = "P3MD7_zFNZaMhgdIwSG9g9Wa0C4bdTBm";
-export const CTP_CLIENT_ID = "zvwPVImt5j1wA1QFjli3IEr3";
-export const CTP_AUTH_URL = "https://auth.europe-west1.gcp.commercetools.com";
-export const CTP_API_URL = "https://api.europe-west1.gcp.commercetools.com";
-export const CTP_SCOPES = "manage_project:essential-items";
+export const CTP_PROJECT_KEY = 'essential-items';
+export const CTP_CLIENT_SECRET = 'P3MD7_zFNZaMhgdIwSG9g9Wa0C4bdTBm';
+export const CTP_CLIENT_ID = 'zvwPVImt5j1wA1QFjli3IEr3';
+export const CTP_AUTH_URL = 'https://auth.europe-west1.gcp.commercetools.com';
+export const CTP_API_URL = 'https://api.europe-west1.gcp.commercetools.com';
+export const CTP_SCOPES = 'manage_project:essential-items';
