6216dbe6af4e3f14a471f868b31dedcca4255527
gogicha007 <gogicha@gmail.com>
2024-05-16 23:24:59 +0400

    feat: check postal code

--- src/app/services/postal-codes.ts
+++ src/app/services/postal-codes.ts
@@ -1,7 +1,11 @@
 import { ITFMap } from '../interfaces/interfaces';
 
 const POSTALS: ITFMap = {
-  us: ['United States', '^\\d{5}(?:[-\\s]\\d{4})?$', 'NNNNN (optionally NNNNN-NNNN)'],
+  us: [
+    'United States',
+    '^\\d{5}(?:[-\\s]\\d{4})?$',
+    'NNNNN (optionally NNNNN-NNNN)',
+  ],
   uk: [
     'United Kingdom',
     '[A-Z]{1,2}[0-9][0-9A-Z]?\\s?[0-9][A-Z]{2}',
@@ -13,7 +17,11 @@ const POSTALS: ITFMap = {
     'ANA NAN',
   ],
   de: ['Germany', '^(?!01000|99999)(0[1-9]\\d{3}|[1-9]\\d{4})$', 'NNNNN'],
-  fr: ['France', '^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B)) *([0-9]{3})?$', 'NN NNN'],
+  fr: [
+    'France',
+    '^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B)) *([0-9]{3})?$',
+    'NN NNN',
+  ],
   nl: ['Netherlands', '\\d{4}[A-Z]{2}', 'NNNN AA'],
 };
 
